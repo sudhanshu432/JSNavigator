d71cbafb669da2681771c5587d63e9bb5d8197ef
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-01 03:15:21 +0530

    Add files via upload

---
+++ 2- Fundamentals of JS I/1- Introduction to code Structure/Practice/index.js
@@ -0,0 +1,2 @@
+// Code Structure of JS Program
+console.log("Heklo welcome of Js"); console.log("Heklo welcome of Js");
---
+++ 2- Fundamentals of JS I/1- Introduction to code Structure/basics.js
@@ -0,0 +1,12 @@
+/** Code Structure of JS Program */
+
+console.log('Hello world!');
+//console.log('Welcome to the Session');
+
+// This is a JavScript Program
+/*hey,
+I 
+am 
+learning*/
+
+
