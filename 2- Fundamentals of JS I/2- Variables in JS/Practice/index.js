a6f8b5a0ef21d918abf01c3d9ae75c6c045c91c1
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-01 14:41:23 +0530

    Add files via upload

---
+++ 2- Fundamentals of JS I/2- Variables in JS/Practice/index.js
@@ -0,0 +1,34 @@
+// Code Structure of JS Program
+console.log("Heklo welcome of Js"); console.log("Heklo welcome of Js");
+
+console.log("123");
+console.log(1+2);
+console.log("1+2");
+
+x = 10;
+let x1 = 10;
+var x2 =10;
+const x3= 10;
+console.log(x+x1+x2+x3);
+
+// Declare & initializing
+let y = 11;
+console.log('y');
+console.log(y);
+
+y = 20;
+console.log(y);
+y = "ten";
+console.log(y);
+y = true;
+console.log(y);
+
+// Declaring
+let z;
+let k, l, m, n;
+let k1, l1=14, m1=13, n1 = 12;
+// Initializing
+z=55;
+
+
+
---
+++ 2- Fundamentals of JS I/2- Variables in JS/variables.js
@@ -0,0 +1,18 @@
+/** Variables in JavaScript! */
+console.log('Some Message');
+console.log(1 + 2);
+
+//Declare & initializing
+let x = 10;
+console.log(x);
+
+x = 20;
+console.log(x);
+
+x = 'ten';
+console.log(x);
+
+//Declaring
+let y,z,a=30,b=40,c=20;
+//Initializing
+y=50;
