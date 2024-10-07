661fbb149860f11b4ecb5f06442d939bcd4a744a
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-07 20:25:13 +0530

    Add files via upload

---
+++ 3- Fundamentals of JS II/2- Comparison Operator/Practice/index.js
@@ -0,0 +1,41 @@
+/** Operators in JS */
+
+// 4. Comparison Operator
+//(==, !=, <, <=, >, >=, ===, !==)
+
+// console.log(5==5);
+// console.log(5==6);
+// console.log(5=='5');
+
+// console.log(5!=6);
+// console.log(5!='6');
+// console.log(5!=5);
+// console.log(5!='5');
+
+// console.log(5<5);
+// console.log(5>=5);
+// console.log(5<=5);
+// console.log(5>4);
+
+// console.log('5'==5);
+// console.log(5==='5');
+// console.log(5===5);
+// console.log(5!='6');
+// console.log(5!='5');
+// console.log(5!=='5');
+// console.log(5!=5);
+
+let birthYear = 18;
+let rightToVote = birthYear>= 18;
+
+console.log(rightToVote);
+
+
+// console.log(4>=2);
+// console.log(4<=2);
+
+
+// 5. Logical Operators (&&, ||, !)
+
+
+
