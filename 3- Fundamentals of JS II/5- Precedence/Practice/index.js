c866d380388e7b9dd8eb2eb208e381f379a91340
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-08 14:57:27 +0530

    Add files via upload

---
+++ 3- Fundamentals of JS II/5- Precedence/Practice/index.js
@@ -0,0 +1,9 @@
+/** Operator precedence */
+
+let m1 = 20;
+let m2 = 15;
+let m3 = 10;
+
+// let avg = m1 + m2 + m3 / 3;
+let avg = (m1 + m2 + m3) / 3;
+console.log(avg);
---
+++ 3- Fundamentals of JS II/5- Precedence/precedence.js
@@ -0,0 +1,8 @@
+/** Operator precedence */
+
+let m1 = 20;
+let m2 = 15;
+let m3 = 10;
+
+let avg = (m1 + m2 + m3) / 3;
+console.log(avg);
