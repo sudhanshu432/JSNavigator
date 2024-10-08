c7aca53c1b135d5ace875efea55b86578900b70c
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-08 15:43:17 +0530

    Add files via upload

---
+++ 3- Fundamentals of JS II/6- Type coercion/Practice/index.js
@@ -0,0 +1,18 @@
+/** Type Coercion */
+
+console.log(5 + 5);
+
+let num = '5' + 5;
+console.log(typeof num);
+
+let fullName = 'Sudhanshu' + ' ' + 'Kumar';
+console.log(fullName);
+
+let sub = '55' - 10;
+console.log(sub);
+console.log(typeof(sub));
+
+console.log('4' * 2);
+
+// Implicit conversion in JavaScript, also known as type coercion, 
+// occurs when the language automatically converts one data type to another during operations.
