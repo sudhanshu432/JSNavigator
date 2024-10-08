66cde29528e9448b88f8effc18d36952481795d2
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-08 18:18:54 +0530

    Add files via upload

---
+++ 3- Fundamentals of JS II/Practice Problems/Q-2 CalculateAverage.js
@@ -0,0 +1,7 @@
+function calculateAverage(student1Score ,student2Score ,student3Score) {
+    let averageScore
+  //Write your code here
+    averageScore = (parseFloat(student1Score) + parseFloat(student2Score) + parseFloat(student3Score))/3;
+  
+  return averageScore;
+  }
