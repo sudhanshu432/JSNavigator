08f638d0100e56b0a4bc4d95c7dd48798e1d90dc
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-09 19:11:07 +0530

    Add files via upload

---
+++ 4- Conditional Statement & Looping/4- Email Validation Intuition/Practice/index.js
@@ -0,0 +1,7 @@
+/** 4- Email Validation Intuition */
+
+// sudhanshukumar432@gmail.com
+// 1. Input an Email
+// 2. Length of the mail>11
+// 3. After last'.' only 2 or 3 Character are allowed
+// 4. Minimum 3 Character between '@' and last '.'
