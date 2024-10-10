76f1c950fed61ca67632a54cb2e0cce06384a719
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-11 02:17:24 +0530

    Add files via upload

---
+++ 4- Conditional Statement & Looping/9- Introduction To For Loop/index.js
@@ -0,0 +1,11 @@
+// Iterations - For Loop
+// print a message 10 times
+
+// for(let i = 0; i<10; i++){
+//   console.log("For loop "+i);
+// }
+
+// Print 1 to 20
+for(let i = 1; i<=20; i++){
+  console.log(i);
+}
