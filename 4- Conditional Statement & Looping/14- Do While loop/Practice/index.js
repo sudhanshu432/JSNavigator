4ad2cb899342a4c8e39c966fd0a4d685baa6d8b5
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-13 22:51:32 +0530

    Add files via upload

---
+++ 4- Conditional Statement & Looping/14- Do While loop/Practice/index.js
@@ -0,0 +1,30 @@
+/** Do- While Loop */
+//Guess the number
+
+// let randomNum = 8;
+
+// let guessedNum;
+
+// do{
+//   guessedNum = prompt("Guess the number: ");
+//   if(randomNum == guessedNum){
+//     console.log("Guessed Correctly!!");
+//   }else{
+//     console.log("Take another guess");
+//   }
+// }while(guessedNum != randomNum);
+
+
+// let randomNum = Math.round(Math.random() * 10);
+// console.log(randomNum);
+
+// let guessedNum;
+
+// do {
+//   guessedNum = prompt('Guess the Number: ');
+//   if (guessedNum == randomNum) {
+//     console.log('Guessed Correctly!!');
+//   } else {
+//     console.log('Take another guess');
+//   }
+// } while (guessedNum != randomNum);
