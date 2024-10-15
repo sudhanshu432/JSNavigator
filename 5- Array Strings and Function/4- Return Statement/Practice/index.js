0ffc37cffd2fc676cea945d51fdc899a26917563
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-15 11:18:57 +0530

    Add files via upload

---
+++ 5- Array Strings and Function/4- Return Statement/Practice/index.js
@@ -0,0 +1,15 @@
+// Return value of a funcion
+
+function squared(number){
+    // console.log(number*number);
+    return number*number;
+    // console.log(number*number);
+}
+
+// squared(5);
+// squared(6);
+// squared(10);
+// squared(16);
+
+const squared5 = squared(5);
+console.log(squared5);
---
+++ 5- Array Strings and Function/4- Return Statement/returnValue.js
@@ -0,0 +1,12 @@
+/** Return value of a function */
+
+function squared(number) {
+  return number * number;
+  console.log(number * number);
+}
+
+const a = squared(5);
+console.log(a);
+squared(15);
+squared(10);
+squared(7);
