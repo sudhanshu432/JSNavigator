3ac069f15e838936efeade0786e2150bb95d6015
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-15 11:17:56 +0530

    Add files via upload

---
+++ 5- Array Strings and Function/2- Functions with Parameters/index.js
@@ -0,0 +1,22 @@
+// Functions in JS
+
+// Creation or defining a function
+// function greetUser(userName, medium){
+//     console.log(`Hello ${userName}, Welcome to the ${medium}`);
+// }
+
+// Call or invoke
+// greetUser("Sudhanshu", "Facebook");
+
+function calculateAge(birthYear){
+    console.log(2024-birthYear);
+}
+function printDetails(name){
+    console.log(`Name: ${name}`);
+    calculateAge(2009);
+}
+
+printDetails("Rahul");
+
+
+
