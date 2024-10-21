bd38b900cda550aaee34bb947ad4fbcc98f36c28
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-21 22:23:33 +0530

    Add files via upload

---
+++ 6- Working of JS/10- More on let and var/Practice/index.js
@@ -0,0 +1,15 @@
+var a = 10;
+let b = 20;
+
+console.log(`a: ${a}, b: ${b}`);
+
+var a = 55;
+console.log(`a: ${a}`);
+
+// let b = 33; // error
+// console.log(b);
+
+// var a = 1;
+// var b = 2;
+
+// console.log(`a: ${a}`);
---
+++ 6- Working of JS/10- More on let and var/scope1.js
@@ -0,0 +1,7 @@
+var a = 10;
+let b = 20;
+
+console.log(`a: ${a}, b: ${b}`);
+
+var a = 55;
+console.log(`a: ${a}`);
