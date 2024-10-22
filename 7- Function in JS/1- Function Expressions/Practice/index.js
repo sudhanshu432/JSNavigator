dc2466c067c3a9736401f268c6cfa47a5abc7291
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-22 12:32:00 +0530

    Add files via upload

---
+++ 7- Function in JS/1- Function Expressions/Practice/index.js
@@ -0,0 +1,23 @@
+/* Functions in JS */
+
+// Function Declaration
+function sum(a, b){
+    return a+b;
+}
+
+const ans = sum(5, 6);
+console.log(ans);
+
+console.log(sum1(2,3));
+
+//Function Declaration
+function sum1(a, b){
+    return a+b;
+}
+
+console.log(sum2(1, 8));
+// Function expression
+const sum2 = function (a, b){
+    return a+b;
+};
+
