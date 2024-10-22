9f668795a6045b7b3bd174e9a27cb9bf337d524b
Sudhanshu Kumar <87382259+sudhanshu432@users.noreply.github.com>
2024-10-23 03:40:41 +0530

    Update index.js

--- 7- Function in JS/3- IIFE/Practice/index.js
+++ 7- Function in JS/3- IIFE/Practice/index.js
@@ -8,3 +8,25 @@
 (function (a,b){
     console.log(a ** b);
 })(2,3); // () make this function invoked when it created
+
+const user = (function(){
+    const userData={
+        userName: "John",
+        userAge: 20,
+    }
+
+    function getName(){
+        console.log(userData.userName);
+    }
+
+    function updateAge(age){
+        console.log(userData.userAge + age);   
+    }
+
+    return {getName, updateAge};
+})();
+
+console.log(user);
+console.log(user.userData); // here user.userData is not returning that's why output will be undefined
+user.getName();
+user.updateAge(3);
